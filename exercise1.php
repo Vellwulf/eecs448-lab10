e8d7e2d41e716939ec60574bd34113a14c992bcc
Yuri Martinez <martinez.moylan@ku.edu>
2021-12-07 15:41:09 -0600

    Create exercise 1 php

---
+++ exercise1.php
@@ -0,0 +1,11 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<title>EECS448 Lab10 Exercise 1</title>
+		<meta name="description" content="Displays the empty tables made for">
+		<meta name="author" content="Yuri Martinez-Moylan">
+	</head>
+</html>
