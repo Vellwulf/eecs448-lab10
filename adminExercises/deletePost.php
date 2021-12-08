d41f14c52474c9824ba3f3dd0bbc73f305e2c0a2
Vellwulf <89924600+Vellwulf@users.noreply.github.com>
2021-12-07 22:35:26 -0600

    Create post deletion back end file

---
+++ adminExercises/deletePost.php
@@ -0,0 +1,50 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<title>EECS448 Lab10 Exercise 7</title>
+		<meta name="description" content="Allows admin to select posts to delete.">
+		<meta name="author" content="Yuri Martinez-Moylan">
+	</head>
+	<style>
+		body {
+			background: LightBlue;
+		}
+		
+		p.ID {
+			text-indent: 15px;
+		}
+	</style>
+	
+	<?php
+		$serverName = "mysql.eecs.ku.edu";
+		$username = "y038m265";
+		$password = "pukau9Ai";
+		$databaseName = "y038m265";
+	
+		// Create connection
+		$conn = new mysqli($serverName, $username, $password, $databaseName);
+
+		// Check connection
+		if ($conn->connect_error) {
+			die("Connection failed: " . $conn->connect_error);
+		}
+		
+		$targetPosts = $_POST["userSelect"];
+	?>
+	
+	<body>
+		<h3>Deletion complete!</h3>
+		
+		<?php
+			forEach($targetPosts as $postID) {
+				echo '<p class="ID">post ID: ' . $postID . '</p>';
+			}
+			
+			$conn -> close();
+		?>
+		
+	</body>
+</html>
