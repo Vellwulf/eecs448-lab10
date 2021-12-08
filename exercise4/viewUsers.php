0496e8e1ca39e6524cc7ffed3e3c9c860ccec105
Vellwulf <89924600+Vellwulf@users.noreply.github.com>
2021-12-07 20:11:46 -0600

    Create view all users webpage

---
+++ exercise4/viewUsers.php
@@ -0,0 +1,35 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<title>EECS448 Lab10 Exercise 3</title>
+		<meta name="description" content="Lets user create a post.">
+		<meta name="author" content="Yuri Martinez-Moylan">
+	</head>
+	<style>
+		body {
+			background: LightBlue;
+		}
+	</style>
+	<body>
+		<?php 
+			$serverName = "mysql.eecs.ku.edu";
+			$username = "y038m265";
+			$password = "pukau9Ai";
+			$databaseName = "y038m265";
+	
+			// Create connection
+			$conn = new mysqli($serverName, $username, $password, $databaseName);
+
+			// Check connection
+			if ($conn->connect_error) {
+				die("Connection failed: " . $conn->connect_error);
+			}
+			
+			echo "connection successful";
+			$conn -> close();
+		?>
+	</body>
+</html>
